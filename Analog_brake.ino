7ba432ae24b44947f4e444629784901e5aa81c55
Simone Bernabè <simone.bernabe@studenti.unitn.it>
2017-07-26 14:51:13 +0200

    File iniziale

---
+++ Analog_brake.ino
@@ -0,0 +1,25 @@
+int init_min = 0;
+int max_read = 0;
+
+void setup(){
+  Serial.begin(115200);
+  init_min = analogRead(A0);
+  pinMode(A0, INPUT);
+}
+
+void loop(){
+  int pot = analogRead(A0);
+  
+  if(pot > max_read){
+    max_read = pot;
+  }
+  
+  int mapped = map(pot,init_min,max_read,0,32727);
+
+  if(mapped < 0){
+    mapped = 0;
+  }
+  
+  delay(25);
+  Serial.println(mapped);
+}
